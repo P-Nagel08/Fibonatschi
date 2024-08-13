5f11c4438761845390967a381eb57a71185afbc3
pascal <pascal-leon.nagel@strauss.de>
2024-08-13 13:16:44 +0200

    Tets-0.1

---
+++ fibonacci.js
@@ -0,0 +1,7 @@
+function calculateFibonacci() {
+    let inputNumber = document.getElementById("inputNumber").value;
+    const resultDiv = document.getElementById("result");
+
+    document.getElementById('result').innerHTML = 'Test123';
+
+}
