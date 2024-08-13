d5abe8fd27d3d69e86c14d5d9ee36a8297c5f6b9
pascal <pascal-leon.nagel@strauss.de>
2024-08-13 14:41:54 +0200

    test-0.03

--- fibonacci.js
+++ fibonacci.js
@@ -2,6 +2,13 @@ function calculateFibonacci() {
     let inputNumber = +document.getElementById("inputNumber").value;
     const resultDiv = document.getElementById("result");
 
-   
+    let fibonacci1 = 0;
+    let fibonacci2 = 1;
+    for (let i = 2; i < inputNumber; i++) {
+        const newFibonacci = fibonacci1 + fibonacci2;
+        fibonacci1 = fibonacci2;
+        fibonacci2 = newFibonacci;
+    }
 
-}
+    resultDiv.innerHTML = output;
+}   
